48d2b3639d404d49cc8bd2642906f53e3ff1979e
Ivan <81016431+IvanMataFernandez@users.noreply.github.com>
2022-09-22 15:26:00 +0200

    Pagina actualizada con CSS tmb

--- app/index.php
+++ app/index.php
@@ -1,3 +1,31 @@
+<html>
+	<link rel="stylesheet" href="estilos.css">
+	
+	<head>
+
+		<h1 class="titulo">  [Titulo de pagina web aqui] </h1>
+	</head>
+	<body>
+
+		<div class="margen">
+			<br>
+			
+			<div class="tituloLogIn"> Iniciar sesión </div>
+			
+			<form> <! -- añadir el action despues y la dirección de la página de registro de datos -->
+				
+				<div class="textoLogIn"> Correo electrónico: </div>
+				<input class="campoLogIn" type="text" name="nombre" value="Nombre de usuario aquí"> <br>
+				<div class="textoLogIn"> Contraseña: </div>				
+				<input class="campoLogIn" type="text" name="nombre" value="Contraseña aquí"> <br> <br>	
+				<input class ="botonReset" type="reset" value="Restablecer valores"> <br> <br>
+				<input class ="botonOpcion" type="submit" value="Iniciar sesión"> <br> <br>
+				<a href="paginaNoExiste.php"><input class="botonOpcion" type="button" value="Registrarse"> </a>
+				
+
+
+		</div>
+
 <?php
   echo '<h1>Yeah, it works!<h1>';
   // phpinfo();
@@ -27,3 +55,6 @@ while ($row = mysqli_fetch_array($query)) {
 }
 
 ?>
+	</body>
+</html>
+
