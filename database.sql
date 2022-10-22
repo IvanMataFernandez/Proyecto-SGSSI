ce16f0260fed7374485a18efbd610d7038d8d470
Ivan <81016431+IvanMataFernandez@users.noreply.github.com>
2022-10-22 17:22:21 +0200

    Add files via upload

--- database.sql
+++ database.sql
@@ -34,7 +34,7 @@ DROP TABLE USUARIOS;
 
 -- Crear tablas
 
-CREATE TABLE USUARIOS (nombre varchar(50), dni varchar(10), telefono varchar(9), nacimiento varchar(10), email varchar(50), usuario varchar(50), contraseña varchar(50), primary key (usuario) );
+CREATE TABLE USUARIOS (nombre varchar(50), dni varchar(10), telefono varchar(9), nacimiento varchar(10), email varchar(50), usuario varchar(50), contraseña varchar(64), seed varchar(10), primary key (usuario) );
 
 CREATE TABLE DATOS (dato1 varchar(25), dato2 varchar(25), dato3 varchar(25), dato4 varchar(25), dato5 varchar(25), primary key (dato1));
 
